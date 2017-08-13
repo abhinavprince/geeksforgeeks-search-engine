a0bec30b6b8c3be88e7c8f05d32280e2ff141936
coderabhishek <abhishekkmr18@gmail.com>
2017-08-13 14:30:17 +0530

    tf-idf indexing working

--- index.py
+++ index.py
@@ -1,5 +1,9 @@
-import bm25
-import tf_idf
+import math
+import pickle
+import os
+from collections import Counter
+
+symbols = [',', '.', ';', '\'', '"', '{', '}', '[', ']', '(', ')', '?', ':', '*', '^', '-', '%']
 
 class GFG:
 
@@ -10,32 +14,52 @@ class GFG:
 		open_file = open(words_file, 'r')
 		words_list =[]
 		contents = open_file.readlines()
-		for i in range(len(contents)):
-			words_list.extend(contents[i].split())
-		return words_list    
-		open_file.close()
 
-	def create(self, documents ,dict):
+		for i in range(len(contents)):
+                    for s in symbols:
+                            contents[i] = contents[i].replace(s, ' ')
+                    words_list.extend(contents[i].split())
+		return words_list    
+
+	def create(self, documents):
+                dict = {}
 		for doc in documents:
 			wordlist = self.index(doc)
 			for word in wordlist:
+                                word = word.lower()
 				if word in dict:
 					dict[word].append(doc)
 				else:
 					dict[word] = []
 					dict[word].append(doc)
-		return dict
+                
+                idf = {}
+                for k in dict:
+                    dict[k].sort()
+                    dict[k] = Counter(dict[k]).items() 
+                    idf[k] = math.log(17444/len(dict[k]))
+                    dict[k] = sorted(dict[k], key = lambda x: x[1])[-9:]
+                    dict[k].reverse()
+                return (dict, idf)
 
 
-dictionary = {}
-documents=[]
-documents.append('1.txt')
-documents.append('2.txt')
+documents=os.listdir("texts")
 obj = GFG()
-dictionary = obj.create(documents, dictionary)
-f = open('a.txt', 'w')
-for i in sorted(dictionary):
-    f.write(i + " ")
-    for j in dictionary[i]:
-    	f.write(j + " ")
-    f.write("\n")
+os.chdir("texts")
+(dictionary, idf) = obj.create(documents)
+print dictionary
+
+#print idf
+#f = open('a.txt', 'w')
+#for i in sorted(dictionary):
+#    f.write(i + " ")
+#    for j in dictionary[i]:
+#    	f.write(j + " ")
+#    f.write("\n")
+os.chdir("..")
+with open("inverted_index.pickle", 'wb') as h:
+        pickle.dump(dictionary, h, protocol=pickle.HIGHEST_PROTOCOL)
+with open("idf.pickle", 'wb') as h:
+        pickle.dump(idf, h, protocol=pickle.HIGHEST_PROTOCOL)
+
+
